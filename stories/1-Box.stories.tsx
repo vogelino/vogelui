d6c23cb37219f6541630906874620f35e1ae6040
Lucas Vogel <contact@vogelino.com>
2019-12-04 00:22:21 +0100

    Use theme color names in box story

--- stories/1-Box.stories.tsx
+++ stories/1-Box.stories.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import Box from '../src/components/Box'
-import makeStory from '../src/utils/storybookUtil'
+import StoryWrapper from '../src/utils/storybookUtil'
 
 export default {
 	title: 'Box',
 }
 
-export const usingBackgroundColor = makeStory(
-	<Box>
-		<Box my={10} backgroundColor="primary" width={100} height={100} />
-		<Box my={10} backgroundColor="red" width={100} height={100} />
-		<Box my={10} backgroundColor="green" width={100} height={100} />
-	</Box>,
+export const usingBackgroundColor = (): ReactNode => (
+	<StoryWrapper>
+		<Box>
+			<Box my={10} backgroundColor="primary" width={100} height={100} />
+			<Box my={10} backgroundColor="secondary" width={100} height={100} />
+			<Box my={10} backgroundColor="error" width={100} height={100} />
+		</Box>
+	</StoryWrapper>
 )
