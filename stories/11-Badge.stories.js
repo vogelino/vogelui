b20fb53ae9a1307a9d53e7d8a6f1158168b4f7b9
Lucas Vogel <contact@vogelino.com>
2020-10-18 20:46:29 +0200

    Badge accept dynamic formats and can be a superscript

--- stories/11-Badge.stories.js
+++ stories/11-Badge.stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Table, TableRow, TableCell, TableHeaderCell } from '../src/components/Table'
 import StoryWrapper from '../src/utils/storybookUtil'
 import { Subhead } from '../src/components/Typography'
+import Button from '../src/components/Button'
 import Badge from '../src/components/Badge'
 
 export default {
@@ -29,19 +30,13 @@ export const variantsAndSizes = () => (
 						<Subhead>Default</Subhead>
 					</TableCell>
 					<TableCell>
-						<Badge variant="default" size="s">
-							Badge
-						</Badge>
+						<Badge variant="default" size="s" content={-120} />
 					</TableCell>
 					<TableCell>
-						<Badge variant="default" size="m">
-							Badge
-						</Badge>
+						<Badge variant="default" size="m" content={-60} />
 					</TableCell>
 					<TableCell>
-						<Badge variant="default" size="l">
-							Badge
-						</Badge>
+						<Badge variant="default" size="l" content={0} />
 					</TableCell>
 				</TableRow>
 				<TableRow>
@@ -49,19 +44,13 @@ export const variantsAndSizes = () => (
 						<Subhead>Primary</Subhead>
 					</TableCell>
 					<TableCell>
-						<Badge variant="primary" size="s">
-							Badge
-						</Badge>
+						<Badge variant="primary" size="s" content={32} />
 					</TableCell>
 					<TableCell>
-						<Badge variant="primary" size="m">
-							Badge
-						</Badge>
+						<Badge variant="primary" size="m" content={0} />
 					</TableCell>
 					<TableCell>
-						<Badge variant="primary" size="l">
-							Badge
-						</Badge>
+						<Badge variant="primary" size="l" content={120} />
 					</TableCell>
 				</TableRow>
 				<TableRow>
@@ -70,19 +59,13 @@ export const variantsAndSizes = () => (
 					</TableCell>
 
 					<TableCell>
-						<Badge variant="error" size="s">
-							Badge
-						</Badge>
+						<Badge variant="error" size="s" content={-120} />
 					</TableCell>
 					<TableCell>
-						<Badge variant="error" size="m">
-							Badge
-						</Badge>
+						<Badge variant="error" size="m" content={-20} />
 					</TableCell>
 					<TableCell>
-						<Badge variant="error" size="l">
-							Badge
-						</Badge>
+						<Badge variant="error" size="l" content={-12} />
 					</TableCell>
 				</TableRow>
 				<TableRow>
@@ -91,19 +74,13 @@ export const variantsAndSizes = () => (
 					</TableCell>
 
 					<TableCell>
-						<Badge variant="warning" size="s">
-							Badge
-						</Badge>
+						<Badge variant="warning" size="s" content={42.55} />
 					</TableCell>
 					<TableCell>
-						<Badge variant="warning" size="m">
-							Badge
-						</Badge>
+						<Badge variant="warning" size="m" content={-66.12} />
 					</TableCell>
 					<TableCell>
-						<Badge variant="warning" size="l">
-							Badge
-						</Badge>
+						<Badge variant="warning" size="l" content={99.99} />
 					</TableCell>
 				</TableRow>
 				<TableRow>
@@ -112,19 +89,197 @@ export const variantsAndSizes = () => (
 					</TableCell>
 
 					<TableCell>
-						<Badge variant="success" size="s">
-							Badge
+						<Badge variant="success" size="s" content={0} />
+					</TableCell>
+					<TableCell>
+						<Badge variant="success" size="m" content={68} />
+					</TableCell>
+					<TableCell>
+						<Badge variant="success" size="l" content={7} />
+					</TableCell>
+				</TableRow>
+			</tbody>
+		</Table>
+	</StoryWrapper>
+)
+
+export const NumberBadgesAndDots = () => (
+	<StoryWrapper>
+		<Table>
+			<tbody>
+				<TableRow>
+					<TableHeaderCell />
+					<TableHeaderCell>
+						<Subhead>With content</Subhead>
+					</TableHeaderCell>
+					<TableHeaderCell>
+						<Subhead>As dot</Subhead>
+					</TableHeaderCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Default</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge content={0} />
+					</TableCell>
+					<TableCell>
+						<Badge />
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Error</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge variant="error" content={10} />
+					</TableCell>
+					<TableCell>
+						<Badge variant="error" />
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Primary</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge variant="primary" content={-20} />
+					</TableCell>
+					<TableCell>
+						<Badge variant="primary" />
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Warning</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge variant="warning" content="Warning" />
+					</TableCell>
+					<TableCell>
+						<Badge variant="warning" />
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Success</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge variant="success" content={-120} />
+					</TableCell>
+					<TableCell>
+						<Badge variant="success" />
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Custom</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge color="#6e00ff" content={120} />
+					</TableCell>
+					<TableCell>
+						<Badge color="#6e00ff" />
+					</TableCell>
+				</TableRow>
+			</tbody>
+		</Table>
+	</StoryWrapper>
+)
+
+export const AsSuperscript = () => (
+	<StoryWrapper>
+		<Table>
+			<tbody>
+				<TableRow>
+					<TableHeaderCell />
+					<TableHeaderCell>
+						<Subhead>On element</Subhead>
+					</TableHeaderCell>
+					<TableHeaderCell>
+						<Subhead>On text</Subhead>
+					</TableHeaderCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Default</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge content={0}>
+							<Button>Button</Button>
 						</Badge>
 					</TableCell>
 					<TableCell>
-						<Badge variant="success" size="m">
-							Badge
+						<Badge dot>Content</Badge>
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Error</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge variant="error">
+							<Button>Button</Button>
 						</Badge>
 					</TableCell>
 					<TableCell>
-						<Badge variant="success" size="l">
-							Badge
+						<Badge variant="error" content="NEW">
+							Content
 						</Badge>
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Primary</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge variant="primary" content={-20}>
+							<Button>Button</Button>
+						</Badge>
+					</TableCell>
+					<TableCell>
+						<Badge variant="primary">Content</Badge>
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Warning</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge variant="warning">
+							<Button>Button</Button>
+						</Badge>
+					</TableCell>
+					<TableCell>
+						<Badge variant="warning" content={12.3}>
+							Content
+						</Badge>
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Success</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge variant="success" content={-120}>
+							<Button>Button</Button>
+						</Badge>
+					</TableCell>
+					<TableCell>
+						<Badge variant="success">Content</Badge>
+					</TableCell>
+				</TableRow>
+				<TableRow>
+					<TableCell>
+						<Subhead>Custom</Subhead>
+					</TableCell>
+					<TableCell>
+						<Badge color="#6e00ff" content={120}>
+							<Button>Button</Button>
+						</Badge>
+					</TableCell>
+					<TableCell>
+						<Badge color="#6e00ff">Content</Badge>
 					</TableCell>
 				</TableRow>
 			</tbody>
