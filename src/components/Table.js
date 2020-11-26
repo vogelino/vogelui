dec1afacbf3b96959d4c4e8c27e733fd33a27409
Lucas Vogel <contact@vogelino.com>
2020-11-27 00:07:49 +0100

    Animate table rows for storybook

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,31 @@
 /** @jsx jsx */
+import { motion } from 'framer-motion'
 import PropTypes from 'prop-types'
 import { jsx, Styled } from 'theme-ui'
 
+const tableAnimation = {
+	initial: { opacity: 0, y: 8 },
+	mount: { opacity: 1, y: 0, transition: { staggerChildren: 0.05 } },
+	unmount: { opacity: 0, y: -8, transition: { staggerChildren: 0.05 } },
+}
+
+const trAnimation = {
+	initial: { opacity: 0, y: 8 },
+	mount: { opacity: 1, y: 0 },
+	unmount: { opacity: 0, y: -8 },
+}
+
 export const Table = ({ children = null, sx = {}, ...props }) => (
-	<Styled.table sx={{ textAlign: 'left', borderSpacing: 0, ...sx }} {...props}>
+	<Styled.table
+		layout
+		variants={tableAnimation}
+		initial="initial"
+		animate="mount"
+		exit="unmount"
+		as={motion.table}
+		sx={{ textAlign: 'left', borderSpacing: 0, ...sx }}
+		{...props}
+	>
 		{children}
 	</Styled.table>
 )
@@ -15,6 +37,8 @@ Table.propTypes = {
 
 export const TableRow = ({ children = null, sx = {}, ...props }) => (
 	<Styled.tr
+		as={motion.tr}
+		variants={trAnimation}
 		sx={{
 			'th:first-of-type, td:first-of-type': {
 				borderRadius: '4px 0 0 4px',
@@ -40,7 +64,9 @@ const cellStyles = {
 	padding: 3,
 }
 export const TableHeaderCell = ({ children = null, sx = {} }) => (
-	<Styled.th sx={{ ...cellStyles, ...sx }}>{children}</Styled.th>
+	<Styled.th as={motion.th} sx={{ ...cellStyles, ...sx }}>
+		{children}
+	</Styled.th>
 )
 
 TableHeaderCell.propTypes = {
@@ -49,7 +75,7 @@ TableHeaderCell.propTypes = {
 }
 
 export const TableCell = ({ children = null, sx = {}, ...props }) => (
-	<Styled.td sx={{ ...cellStyles, ...sx }} {...props}>
+	<Styled.td as={motion.td} sx={{ ...cellStyles, ...sx }} {...props}>
 		{children}
 	</Styled.td>
 )
